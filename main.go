34dddef935c0d31786e3c76a8ffc6ca3b83ef360
bestbug <bestbug@localhost>
2018-09-08 17:10:37 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	rprop "github.com/bestbug456/gorpropplus"
+	gorest "github.com/fredmaggiowski/gorest"
+
+	"gopkg.in/mgo.v2"
+)
+
+var heroID map[string]int
+
+func init() {
+	heroID = make(map[string]int)
+	for i := 0; i < len(heros); i++ {
+		heroID[heros[i]] = i
+	}
+}
+
+func main() {
+
+	address := os.Getenv("address")
+	username := os.Getenv("username")
+	password := os.Getenv("password")
+	option := os.Getenv("option")
+	ssl := os.Getenv("ssl")
+
+	var s *mgo.Session
+	var err error
+	log.Printf("Accessing to db\n")
+	if ssl == "false" {
+		log.Printf("Accessing to db via mgo.Dial\n")
+		s, err = mgo.Dial(fmt.Sprintf("mongodb://%s:%s@%s/%s", username, password, address, option))
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			os.Exit(1)
+		}
+	} else {
+		log.Printf("Accessing to db via ssl\n")
+		s, err = DialUsingSSL(address, option, username, password)
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			os.Exit(1)
+		}
+	}
+	defer s.Close()
+
+	// Create a new handler
+	handler := gorest.NewHandler()
+
+	// Define and setup your custom structures.
+	var nnRes NNResource
+	var statRes StatisticsResource
+	go updateDatabaseInfosPeriodically(s, &nnRes, &statRes)
+
+	var herosRes HeroResource
+
+	// Register the routes.
+	handler.SetRoutes([]*gorest.Route{
+		gorest.NewRoute(&nnRes, "/nn/predict"),
+		gorest.NewRoute(&herosRes, "/heros"),
+		gorest.NewRoute(&statRes, "/stats"),
+	})
+
+	// Get the handler for your HTTP(S) server.
+	router := handler.GetMuxRouter(nil)
+	log.Printf("serving on localhost:8080\n")
+	http.ListenAndServe("localhost:8080", router)
+}
+
+func getActualNewNeuralNetwork(s *mgo.Session) (*rprop.NeuralNetwork, error) {
+	var NN rprop.NeuralNetwork
+	err := s.DB("neuralnetwork").C("weights").Find(nil).One(&NN)
+	if err != nil {
+		return nil, err
+	}
+	return &NN, nil
+}
+
+func getStatistics(s *mgo.Session) (*NNStats, error) {
+	var stats NNStats
+	err := s.DB("neuralnetwork").C("score").Find(nil).One(&stats)
+	if err != nil {
+		return nil, err
+	}
+	return &stats, nil
+}
+
+func updateDatabaseInfosPeriodically(s *mgo.Session, nnRes *NNResource, statRes *StatisticsResource) {
+	for {
+		NN, err := getActualNewNeuralNetwork(s)
+		if err != nil {
+			log.Printf("%s\n", err.Error())
+			os.Exit(1)
+		}
+		NN.ActivationFunction = rprop.Logistic
+		NN.DerivateActivation = rprop.DerivateLogistic
+		NN.ErrorFunction = rprop.SSE
+		NN.DerivateError = rprop.DerivateSSE
+		nnRes.nn = NN
+
+		stats, err := getStatistics(s)
+		if err != nil {
+			log.Printf("%s\n", err.Error())
+			os.Exit(1)
+		}
+		statRes.Stats = stats
+
+		time.Sleep(5 * time.Minute)
+	}
+}
+
+func DialUsingSSL(addresses string, dboption string, username string, password string) (*mgo.Session, error) {
+	listaddresses := make([]string, 0)
+	for _, str := range strings.Split(addresses, ",") {
+		if str != "" {
+			listaddresses = append(listaddresses, str)
+		}
+	}
+	dboptions := strings.Split(dboption, "=")
+	if len(dboption) < 2 {
+		return nil, fmt.Errorf("can not found authSource keyword in order to permit SSL connection, aborting")
+	}
+	tlsConfig := &tls.Config{}
+	dialInfo := &mgo.DialInfo{
+		Addrs:    listaddresses,
+		Database: dboptions[1],
+		Username: username,
+		Password: password,
+	}
+
+	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+		return conn, err
+	}
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		return nil, err
+	}
+	session.EnsureSafe(&mgo.Safe{
+		W:     1,
+		FSync: false,
+	})
+	return session, nil
+}
---
+++ resources.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"math/rand"
+
+	rprop "github.com/bestbug456/gorpropplus"
+	gorest "github.com/fredmaggiowski/gorest"
+)
+
+type NNResource struct {
+	nn *rprop.NeuralNetwork
+}
+
+type PredictionRequest struct {
+	Heros []string
+}
+
+type PredictionResponse struct {
+	Win  int
+	Prob float64
+}
+
+func (p *NNResource) Post(r *http.Request) (int, gorest.Response) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Can't read the body: %s.\n", err.Error())
+		return http.StatusInternalServerError, nil
+	}
+
+	var request PredictionRequest
+
+	err = json.Unmarshal(b, &request)
+	if err != nil {
+		log.Printf("Can't marshal heros: %s.\n", err.Error())
+		return http.StatusInternalServerError, nil
+	}
+
+	picks := make([]int, 10)
+	for i := 0; i < len(request.Heros); i++ {
+		picks[i] = heroID[request.Heros[i]]
+	}
+	input := orderPickByTeamAndCreateBitmask(picks)
+
+	ris, err := p.nn.Predict(input)
+	if err != nil {
+		log.Printf("Can't marshal heros: %s.\n", err.Error())
+		return http.StatusInternalServerError, nil
+	}
+
+	var prob float64
+	var win int
+	if int(ris[0]) == 0 {
+		prob = 1 - ris[0]
+	} else {
+		win = 1
+		prob = ris[0]
+	}
+
+	out, err := json.Marshal(PredictionResponse{
+		Prob: prob,
+		Win:  win,
+	})
+	if err != nil {
+		log.Printf("Can't marshal heros: %s.\n", err.Error())
+		return http.StatusInternalServerError, nil
+	}
+	response := gorest.NewStandardResponse()
+	response.SetBody(out)
+
+	return http.StatusOK, response
+}
+
+func orderPickByTeamAndCreateBitmask(picks []int) []float64 {
+	team1Pick := []int{
+		picks[0],
+		picks[3],
+		picks[5],
+		picks[7],
+		picks[8],
+	}
+	team2Pick := []int{
+		picks[1],
+		picks[2],
+		picks[4],
+		picks[6],
+		picks[9],
+	}
+	bitmasks := createBitmasksForTeam(team1Pick)
+	supp := createBitmasksForTeam(team2Pick)
+	bitmasks = append(bitmasks, supp...)
+	return bitmasks
+}
+
+func createBitmasksForTeam(team []int) []float64 {
+	bitmasks := make([]float64, 115)
+	for i := 0; i < len(team); i++ {
+		bitmasks[team[i]] = 1
+	}
+	return bitmasks
+}
+
+type HeroResource struct{}
+
+func (p *HeroResource) Get(r *http.Request) (int, gorest.Response) {
+
+	// Return the array of avaiable dota heros
+	out, err := json.Marshal(heros)
+	if err != nil {
+		log.Printf("Can't marshal heros: %s.\n", err.Error())
+		return http.StatusInternalServerError, nil
+	}
+	response := gorest.NewStandardResponse()
+	response.SetBody(out)
+	return http.StatusOK, response
+}
+
+type StatisticsResource struct {
+	Stats *NNStats
+}
+
+type stats struct {
+	Accuracy float64
+}
+
+func (p *StatisticsResource) Get(r *http.Request) (int, gorest.Response) {
+
+	var statistics stats
+
+	statistics.Accuracy = p.Stats.MatrixQA[0] / p.Stats.MatrixQA[1]
+
+	out, err := json.Marshal(statistics)
+	if err != nil {
+		log.Printf("Can't marshal stats: %s.\n", err.Error())
+		return http.StatusInternalServerError, nil
+	}
+	response := gorest.NewStandardResponse()
+	response.SetBody(out)
+	return http.StatusOK, response
+}
